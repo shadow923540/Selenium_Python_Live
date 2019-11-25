617af706fca46fb51bd959f9081b3e05cd68f035
zuczkows <pz921@wp.pl>
2019-11-25 20:11:52 +0100

    Add Allure step to LoginPage

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -4,7 +4,6 @@ from utilities.util import Util
 from base.basepage import BasePage
 import allure
 
-
 class LoginPage(BasePage):
     log = cl.customLogger(logging.DEBUG)
 
@@ -19,24 +18,27 @@ class LoginPage(BasePage):
     _welcome_message = "//h1[contains(text(), 'Welcome')]"
 
     email = "m.debski+frontend_tests@livechatinc.com"
-    password= "test1@3$"
+    password = "test1@3$"
 
-
-    @allure.step("Type into EmailField - email")
+    @allure.step("Type into EmailField")
     def enterEmail(self, email):
         self.sendKeys(email, self._email_field)
+
+    @allure.step("Type into passwordField")
     def enterPassword(self, password):
         self.sendKeys(password, self._password_field)
+
+    @allure.step("Click LoginButton")
     def clickLoginButton(self):
         self.elementClick(self._login_button, locatorType='xpath')
 
     def login(self, email= email , password = password):
-        self.elementClear(self._email_field)
+        self.waitForElement(self._email_field)
         self.enterEmail(email)
-        self.elementClear(self._password_field)
         self.enterPassword(password)
         self.clickLoginButton()
 
+    @allure.step("Wait for welcomeMessage after login and save message to variable")
     def getWelcomeMessage(self, welcomeMessageToVerify):
         self.waitForElement(self._welcome_message, locatorType='xpath')
         welcomeMessage = self.getText(self._welcome_message, locatorType='xpath')
