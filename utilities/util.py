f9ae12830314e0c9443f932fb968d99ad4a6289a
zuczkows <pz921@wp.pl>
2019-11-25 17:51:49 +0100

    Add Util Class

---
+++ utilities/util.py
@@ -0,0 +1,28 @@
+import utilities.custom_logger as cl
+import logging
+
+class Util():
+
+    log = cl.customLogger(logging.INFO)
+
+
+    def verifyTextContains(self, actualText, expectedText):
+        self.log.info("Actual Text  --> :: " + actualText)
+        self.log.info("Expected Text  --> :: " + expectedText)
+        if expectedText.lower() in actualText.lower():
+            self.log.info("### VERIFICATION CONTAINS !!!")
+            return True
+        else:
+            self.log.info("### VERIFICATION DOES NOT CONTAINS !!!")
+            return False
+
+    def verifyTextMatch(self, actualText, expectedText):
+        self.log.info("Actual Text --> :: " + actualText)
+        self.log.info("Expected Text  --> :: " + expectedText)
+        if actualText.lower() == expectedText.lower():
+            self.log.info("### VERIFICATION MATCHED !!!")
+            return True
+        else:
+            self.log.info("### VERIFICATION DOES NOT MATCHED !!!")
+            return False
+
