33c67661d1ec7dc66b991991bd773185914fa970
shadow923540 <Naruto#sasuke123!>
2019-11-28 14:18:57 +0100

    add singleton for driver

--- base/webdriverfactory.py
+++ base/webdriverfactory.py
@@ -1,31 +1,19 @@
 from selenium import webdriver
 import os
 
+def singleton(myClass):
+    instances = {}
+    def getInstance(*args, **kwargs):
+        if myClass not in instances:
+            instances[myClass] = myClass(*args,**kwargs)
+        return instances[myClass]
+    return getInstance
+
+@singleton
 class WebDriverFactory():
-    def __init__(self, browser):
-        self.browser = browser
-
+    driverLocation = "C:\\Python_training\\venv\\Scripts\\chromedriver.exe"
+    # driverLocation = "C:\\Users\\Pawel\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\chromedriver.exe"
+    os.environ["webdriver.chrome.driver"] = driverLocation
+    driver = webdriver.Chrome(driverLocation)
     def getWebDriverInstance(self):
-        baseUrl = "https://accounts.labs.livechatinc.com"
-        if self.browser == "chrome":
-            # driverLocation = "C:\\Python_training\\venv\\Scripts\\chromedriver.exe"
-            driverLocation = "C:\\Users\\Pawel\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\chromedriver.exe"
-            os.environ["webdriver.chrome.driver"] = driverLocation
-            driver = webdriver.Chrome(driverLocation)
-        elif self.browser == "firefox":
-            driver = webdriver.Firefox(executable_path="C:\\Python_training\\venv\\Scripts\\geckodriver.exe")
-        elif self.browser == "iexplorer":
-            driverLocation = "C:\\Python_training\\venv\\Scripts\\IEDriverServer.exe"
-            # driverLocation = "C:\\Users\\Pawel\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\chromedriver.exe"
-            os.environ["webdriver.ie.driver"] = driverLocation
-            driver = webdriver.Ie(driverLocation)
-        else:
-            driverLocation = "C:\\Python_training\\venv\\Scripts\\chromedriver.exe"
-            # driverLocation = "C:\\Users\\Pawel\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\chromedriver.exe"
-            os.environ["webdriver.chrome.driver"] = driverLocation
-            driver = webdriver.Chrome(driverLocation)
-
-        driver.maximize_window()
-        driver.implicitly_wait(3)
-        driver.get(baseUrl)
-        return driver
+        return self.driver
