b0f6099b235253d3e6c0ba4f29cb0b06ea3dc51e
zuczkows <pz921@wp.pl>
2019-11-28 22:19:49 +0100

    Add allure steps and chatRatingFilter function

--- pages/Archives.py
+++ pages/Archives.py
@@ -1,17 +1,14 @@
 import utilities.custom_logger as cl
 import logging
-
-from base.basepage import BasePage
 import allure
 import time
+from base.basepage import BasePage
 
 class ArchivesPage(BasePage):
     log = cl.customLogger(logging.DEBUG)
 
     def __init__(self, driver):
         super().__init__(driver)
-        # self.util = Util()
-
 
     ################### LOCATORS #####################
 
@@ -53,8 +50,9 @@ class ArchivesPage(BasePage):
     _rating_field_not_rated = ".css-14igyak > li:nth-child(2)"
     _rating_field_rated_bad = ".css-14igyak > li:nth-child(3)"
     _rating_field_rated_good = ".css-14igyak > li:nth-child(4)"
-    _rating_field_rated_bad_commented = ".css-14igyak > li:nth-child(5)"
-    _rating_field_good_commented =  ".css-14igyak > li:nth-child(6)"
+    _rating_field_rated_and_commented = ".css-14igyak > li:nth-child(5)"
+    _rating_field_bad_commented = ".css-14igyak > li:nth-child(6)"
+    _rating_field_good_commented = ".css-14igyak > li:nth-child(7)"
 
     #Tag field options
     _tag_field_not_tagged = ".css-14igyak > li:nth-child(1)"
@@ -72,11 +70,12 @@ class ArchivesPage(BasePage):
 
     def checkIfUserOnArchivesPage(self):
         self.waitForElementAndCheckText(self._archives_welcome_message, 'css', 'Archives')
-
     def clickAddFilter(self):
         self.elementClick(self._add_filter)
     def clickDateField(self):
         self.elementClick(self._date_field)
+    def clickRatingField(self):
+        self.elementClick(self._rating_field)
     def clickAgentField(self):
         self.elementClick(self._agent_field)
     def clickTagField(self):
@@ -90,14 +89,19 @@ class ArchivesPage(BasePage):
     def getNumbersOfChats(self):
         self.waitUntilElementIsVisible(self._numbers_of_chats, 'css')
         numbers_of_chats = len(self.getElementList(self._numbers_of_chats))
+        self.log.info("### Numbers of chat ::  " + str(numbers_of_chats))
         return numbers_of_chats
     def clickDateFieldAndChoseFilter(self, locator, locatorType = 'css'):
         self.clickDateField()
+        self.elementClick(locator, locatorType)
+    def clickRatingFieldAndChoseFilter(self, locator, locatorType = 'css'):
+        self.clickRatingField()
         self.elementClick(locator, locatorType)
     def clickTagFieldAndChoseFilter(self, locator, locatorType = 'css'):
         self.clickTagField()
         self.elementClick(locator, locatorType)
     def checkTextInDateFieldAndButtonText(self, dateText, buttonText):
+        time.sleep(1)
         result_1 = self.waitForElementAndCheckText(self._show_chats, 'css', buttonText)
         result_2 = self.waitForElementAndCheckText(self._date_field_check_filter, 'css', dateText)
         result = [result_1, result_2]
@@ -112,11 +116,14 @@ class ArchivesPage(BasePage):
 
     def assertResult(self, actual, value):
         if actual == value:
+            self.log.info("### VERIFICATION SUCCESSFUL : " + str(actual) + str(value))
             return "PASS"
-        return "FAIL"
+        else:
+            self.log.error("### VERIFICATION FAIL :: Actual value: " + str(actual) + " should be: " + str(value))
+            return "FAIL"
 
-
-    def checkDaysFilter(self):
+    @allure.step("Click each date option in sequence and check text in webelement - button and filtr")
+    def checkDateFilter(self):
         self.clickAddFilter()
         self.clickDateFieldAndChoseFilter(self._date_field_today)
         result_1 = self.checkTextInDateFieldAndButtonText('Today', 'Show 0 chats')
@@ -132,9 +139,30 @@ class ArchivesPage(BasePage):
         result_6 = self.checkTextInDateFieldAndButtonText('Current month', 'Show 0 chats')
         self.clearAllFilterAndCheckChatsMessage()
         self.waitUntilElementIsClickable(self._show_chats)
+        time.sleep(1)
         self.elementClick(self._show_chats)
         numberOfChats = self.getNumbersOfChats()
         result_7 = self.assertResult(numberOfChats,4)
+        result = [result_1, result_2, result_3, result_4, result_5, result_6, result_7]
+        return result
+
+    @allure.step("Click each rating option in sequence and check text in webelement - button and filtr")
+    def checkRatingFilter(self):
+        self.clickAddFilter()
+        self.clickRatingFieldAndChoseFilter(self._rating_field_any_rating)
+        result_1 = self.checkTextInDateFieldAndButtonText('Any rating', 'Show 3 chats')
+        self.clickRatingFieldAndChoseFilter(self._rating_field_not_rated)
+        result_2 = self.checkTextInDateFieldAndButtonText('Not rated', 'Show 1 chats')
+        self.clickRatingFieldAndChoseFilter(self._rating_field_rated_bad)
+        result_3 = self.checkTextInDateFieldAndButtonText('Rated bad', 'Show 1 chats')
+        self.clickRatingFieldAndChoseFilter(self._rating_field_rated_good)
+        result_4 = self.checkTextInDateFieldAndButtonText('Rated good', 'Show 2 chats')
+        self.clickRatingFieldAndChoseFilter(self._rating_field_rated_and_commented)
+        result_5 = self.checkTextInDateFieldAndButtonText('Rated & commented', 'Show 2 chats')
+        self.clickRatingFieldAndChoseFilter(self._rating_field_bad_commented)
+        result_6 = self.checkTextInDateFieldAndButtonText('Rated bad & commented', 'Show 1 chats')
+        self.clickRatingFieldAndChoseFilter(self._rating_field_good_commented)
+        result_7 = self.checkTextInDateFieldAndButtonText('Rated good & commented', 'Show 1 chats')
         result = [result_1, result_2, result_3, result_4, result_5, result_6, result_7]
         return result
 
@@ -164,18 +192,19 @@ class ArchivesPage(BasePage):
         self.elementClick(self._show_chats)
         self.waitUntilElementIsVisible(self._archives_text)
         result = self.checkIfSelectedTagIsVisibleInConversation(tagname)
-        print(result)
         return result
 
-    def checkSpamFilter(self):
+    @allure.step("Select Spam filter tag then check if Spam tag exists in each displayed conversation")
+    def checkTagSpamFilter(self):
         result = self.checkIfFilterWorkCorrectlyinAllConversation(self._tag_field_spam, 'spam')
         return result
 
-    def checkSalesFilter(self):
+    @allure.step("Select Sales filter tag then check if Spam tag exists in each displayed conversation")
+    def checkTagSalesFilter(self):
         result = self.checkIfFilterWorkCorrectlyinAllConversation(self._tag_field_sales, 'sales')
         return result
 
-
+    @allure.step("Removed all filters")
     def removeAllFilters(self):
         self.elementClick(self._home_icon)
         self.elementClick(self._archives_icon)
